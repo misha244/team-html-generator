29aa85288ca72eaf1bad75a405da3b41f872b62c
Misha <73886737+misha244@users.noreply.github.com>
2021-05-13 18:07:53 +0100

    add intern tests

--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -1,1 +1,42 @@
 const Intern = require("../src/lib/Intern");
+
+describe("Intern", () => {
+  const internInfo = {
+    employeeName: "Corrado",
+    employeeId: "420",
+    employeeEmail: "unclejun@yahoo.com",
+    school: "Top Business School",
+  };
+
+  it("should set up school via constructor fn", () => {
+    const expected = internInfo.school;
+    const intern = new Intern(internInfo);
+    const actual = intern.school;
+
+    expect(actual).toBe(expected);
+  });
+
+  it("should get school when called", () => {
+    const expected = internInfo.school;
+    const intern = new Intern(internInfo);
+    const actual = intern.getSchool();
+
+    expect(actual).toBe(expected);
+  });
+
+  it("should override employeeRole via constructor", () => {
+    const expected = "Intern";
+    const intern = new Intern(internInfo);
+    const actual = intern.employeeRole;
+
+    expect(actual).toBe(expected);
+  });
+
+  it("should get employeeRole when called", () => {
+    const expected = "Intern";
+    const intern = new Intern(internInfo);
+    const actual = intern.getRole();
+
+    expect(actual).toBe(expected);
+  });
+});
