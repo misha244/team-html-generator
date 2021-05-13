ea2721f1d6acbe872465211504d4d0a12838dda9
Misha <73886737+misha244@users.noreply.github.com>
2021-05-13 17:15:10 +0100

    add tests for engineer github and role

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -8,10 +8,34 @@ describe("Engineer", () => {
     github: "unclejun",
   };
 
-  it("should set up github through constructor fn", () => {
+  it("should set up github via constructor fn", () => {
     const expected = engineerInfo.github;
     const engineer = new Engineer(engineerInfo);
     const actual = engineer.github;
+
+    expect(actual).toBe(expected);
+  });
+
+  it("should get GitHub when called", () => {
+    const expected = engineerInfo.github;
+    const engineer = new Engineer(engineerInfo);
+    const actual = engineer.getGitHub();
+
+    expect(actual).toBe(expected);
+  });
+
+  it("should override employeeRole via constructor", () => {
+    const expected = "Engineer";
+    const engineer = new Engineer(engineerInfo);
+    const actual = engineer.employeeRole;
+
+    expect(actual).toBe(expected);
+  });
+
+  it("should get employeeRole when called", () => {
+    const expected = "Engineer";
+    const engineer = new Engineer(engineerInfo);
+    const actual = engineer.getRole();
 
     expect(actual).toBe(expected);
   });
--- src/lib/engineer.js
+++ src/lib/engineer.js
@@ -4,10 +4,15 @@ class Engineer extends Employee {
   constructor({ github }) {
     super(github);
     this.github = github;
+    this.employeeRole = "Engineer";
   }
 
   getGitHub() {
     return this.github;
+  }
+
+  getRole() {
+    return this.employeeRole;
   }
 }
 
