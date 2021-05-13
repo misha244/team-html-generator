8bdadf5ae33f116194ab0f25f8e32984b7a88818
Misha <73886737+misha244@users.noreply.github.com>
2021-05-13 17:02:36 +0100

    add describe and github test

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -1,1 +1,18 @@
-const Employee = require("../src/lib/Engineer");
+const Engineer = require("../src/lib/Engineer");
+
+describe("Engineer", () => {
+  const engineerInfo = {
+    employeeName: "Corrado",
+    employeeId: "420",
+    employeeEmail: "unclejun@yahoo.com",
+    github: "unclejun",
+  };
+
+  it("should set up github through constructor fn", () => {
+    const expected = engineerInfo.github;
+    const engineer = new Engineer(engineerInfo);
+    const actual = engineer.github;
+
+    expect(actual).toBe(expected);
+  });
+});
