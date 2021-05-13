c63855978fe3c575a64e9646cd140a8021ddba2f
Misha <73886737+misha244@users.noreply.github.com>
2021-05-13 14:52:49 +0100

    add rest of properties&methods

--- src/lib/employee.js
+++ src/lib/employee.js
@@ -1,10 +1,25 @@
 class Employee {
-  constructor({ employeeName }) {
+  constructor({ employeeName, employeeId, employeeEmail }) {
     this.employeeName = employeeName;
+    this.employeeId = employeeId;
+    this.employeeEmail = employeeEmail;
+    this.employeeRole = "Employee";
   }
 
   getName() {
     return this.employeeName;
+  }
+
+  getId() {
+    return this.employeeId;
+  }
+
+  getEmail() {
+    return this.employeeEmail;
+  }
+
+  getRole() {
+    return this.employeeRole;
   }
 }
 
