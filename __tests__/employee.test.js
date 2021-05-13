fa9e5c7a2b184b23918b298ea4bd6cb8a1d55258
Misha <73886737+misha244@users.noreply.github.com>
2021-05-13 13:46:06 +0100

    wrote test for employeeName

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,5 +1,17 @@
 const Employee = require("../src/lib/Employee");
 
-test("Hello", () => {
-  expect(1).toBe(1);
+describe("Employee", () => {
+  const employeeInfo = {
+    employeeName: "Corrado",
+    employeeId: "420",
+    employeeEmail: "unclejun@yahoo.com",
+  };
+
+  it("new instance of Employee should be named after a character in The Sopranos", () => {
+    const expected = employeeInfo.employeeName;
+    const employee = new Employee(employeeInfo);
+    const actual = employee.employeeName;
+
+    expect(actual).toBe(expected);
+  });
 });
