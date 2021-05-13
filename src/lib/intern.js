5c25cc72329a9cf258fb7d934541c34b8c0e56d7
Misha <73886737+misha244@users.noreply.github.com>
2021-05-13 17:27:15 +0100

    add properies and methods

--- src/lib/intern.js
+++ src/lib/intern.js
@@ -1,7 +1,19 @@
 const Employee = require("./Employee");
 
 class Intern extends Employee {
-  constructor({}) {}
+  constructor({ school }) {
+    super(school);
+    this.school = school;
+    this.employeeRole = "Intern";
+  }
+
+  getSchool() {
+    return this.school;
+  }
+
+  getRole() {
+    return this.employeeRole;
+  }
 }
 
 module.exports = Intern;
