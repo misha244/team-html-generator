c95b9222a685f815d45284d40ea6c86de4bc711b
Misha <73886737+misha244@users.noreply.github.com>
2021-05-16 11:39:44 +0100

    add req modules

---
+++ src/utils/getAllEmployees.js
@@ -0,0 +1,4 @@
+const createIntern = require("./createIntern");
+const createManager = require("./createManager");
+const createEngineer = require("./createEngineer");
+const getAnswers = require("./getAnswers");
